3e765465251798f077ea35e228331a364758761d
Sergio Blanco <sergio@qrvey.com>
2018-07-19 12:26:03 -0500

    serverjs not in es6

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const app = express();
+'use strict';
+
+var express = require('express');
+var app = express();
 
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
